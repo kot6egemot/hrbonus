f748272c91c50c790a03747e3dd7cfdf8c69ca6b
kot6egemot <Exos93009!!>
2021-01-22 16:40:16 +0300

    fix double

--- base/views/bonus_view.py
+++ base/views/bonus_view.py
@@ -14,10 +14,10 @@ class BonusView(APIView):
         fields = Bonuses_Summary.get_model_fields()
         headers = [{'text': field, 'value': field} for field in fields] + [{'text': 'Actions', 'value': 'Actions'}]
         serialize = BonusSerializer(bonuses, many=True)
-        return JsonResponse({"result": True, "_bonus": serialize.data, "__bonus": serialize.data, 'bonus_headers': headers})
+        return JsonResponse({"result": True, "bonus": serialize.data, 'bonus_headers': headers})
 
     def post(self, request):
         bonus_item = request.data
         bonus_item["PositionFK"]  = randint(1, 100)
         del bonus_item["changed"]
-        return JsonResponse({"result": True, "_item": bonus_item,  "__item": bonus_item})
+        return JsonResponse({"result": True, "item": bonus_item})
