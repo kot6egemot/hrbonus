dac5b5a26a35e809b7a5b4ec4328323ebbba6112
kot6egemot <Exos93009!!>
2021-01-27 16:58:22 +0300

    separate list and dict

--- base/views/bonus_view.py
+++ base/views/bonus_view.py
@@ -6,18 +6,33 @@ from rest_framework.views import APIView
 from base.models import Bonuses_Summary
 from base.serializers.bonus_serializer import BonusSerializer
 
+def delete_props(bonus):
+    del bonus['changed']
+    return bonus
 
 class BonusView(APIView):
 
     def get(self, request):
         bonuses = Bonuses_Summary.objects.all()
         fields = Bonuses_Summary.get_model_fields()
+        editable_columns = ["FirstName", "LastName"]
         headers = [{'text': field, 'value': field} for field in fields] + [{'text': 'Actions', 'value': 'Actions'}]
         serialize = BonusSerializer(bonuses, many=True)
-        return JsonResponse({"result": True, "bonus": serialize.data, 'headers': headers})
+        return JsonResponse(
+            {
+                "result": True,
+                "bonus": serialize.data,
+                'headers': headers,
+                "editable_columns": editable_columns
+            }
+        )
 
     def post(self, request):
         bonus_item = request.data
-        bonus_item["PositionFK"]  = randint(1, 100)
-        del bonus_item["changed"]
-        return JsonResponse({"result": True, "item": bonus_item})
+        if isinstance(bonus_item, list):
+            bonus_item = [delete_props(bonus) for bonus in bonus_item]
+            return JsonResponse({"result": True, "items": bonus_item})
+        else:
+            delete_props(bonus_item)
+            bonus_item["PositionFK"] = randint(1, 100)
+            return JsonResponse({"result": True, "item": bonus_item})
