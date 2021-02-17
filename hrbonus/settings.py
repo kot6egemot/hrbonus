0a64195fd76d84eda790b4a190dada60418eaa9a
kot6egemot <Exos93009!!>
2021-02-17 17:55:47 +0300

    fix

--- hrbonus/settings.py
+++ hrbonus/settings.py
@@ -142,3 +142,22 @@ REST_FRAMEWORK = {
 }
 
 APPEND_SLASH = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
