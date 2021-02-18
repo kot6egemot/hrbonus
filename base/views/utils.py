c8fbfd7ccea95af39811ec732f9d8e838b98e4ec
kot6egemot <Exos93009!!>
2021-02-18 12:27:04 +0300

    fix

--- base/views/utils.py
+++ base/views/utils.py
@@ -6,7 +6,7 @@ class BaseGenericListView:
     _serialize = None
     _hide_columns = []
 
-    def get(self):
+    def get(self, request):
         entity = self._model.objects.all()
         columns = self._model.get_columns(hide_columns=self._hide_columns)
         prop_columns = {
