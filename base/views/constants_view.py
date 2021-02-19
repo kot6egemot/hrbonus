1da1b2998b4f9357fe09b73688b8ffa82662ffdd
kot6egemot <Exos93009!!>
2021-02-19 15:43:49 +0300

    save all entity

--- base/views/constants_view.py
+++ base/views/constants_view.py
@@ -13,5 +13,26 @@ class ConstantLineViewGenericListView(BaseGenericListView):
     _serialize = ConstantsSerializer
 
 
+def save_constant_item(constant_data):
+    constant = Constant.objects.filter(Year=constant_data['Year'], Month=constant_data['Month']).first()
+    constant.PersPart = constant_data['PersPart']
+    constant.LeadMultiplier = constant_data['LeadMultiplier']
+    constant.extMultiplier = constant_data['extMultiplier']
+    return constant_data
+
+
 class ConstantView(APIView, ConstantLineViewGenericListView):
-    pass
+    def post(self, request):
+        constant_item = request.data
+
+        if isinstance(constant_item, list):
+            for constant in constant_item:
+                save_constant_item(constant)
+            constants = Constant.objects.all()
+            serialize = ConstantsSerializer(constants, many=True)
+            return JsonResponse({"result": True, "items": serialize.data})
+        else:
+            constant = save_constant_item(constant_item)
+            serialize = ConstantsSerializer(constant)
+            return JsonResponse({"result": True, "item": serialize.data})
+
--- base/views/lines_view.py
+++ base/views/lines_view.py
@@ -8,6 +8,16 @@ from base.serializers.bonus_serializer import LinesSerializer
 from base.views.utils import delete_props, BaseGenericListView
 
 
+def save_line_item(line_date):
+    line = Lines.objects.get(LineId=line_date['LineId'])
+    line.EffectivePlan = line_date['EffectivePlan']
+    line.EffectiveFact = line_date['EffectiveFact']
+    line.ErrorPlan = line_date['ErrorPlan']
+    line.ErrorFact = line_date['ErrorFact']
+    line.Decision = line_date['Decision']
+    line.save()
+    return line
+
 class LinesViewGenericListView(BaseGenericListView):
     _model = Lines
     _param_entity = 'line'
@@ -19,9 +29,14 @@ class LinesView(APIView, LinesViewGenericListView):
 
     def post(self, request):
         line_item = request.data
+
         if isinstance(line_item, list):
-            line_item = [delete_props(line) for line in line_item]
-            return JsonResponse({"result": True, "items": line_item})
+            for line in line_item:
+                save_line_item(line)
+            lines = Lines.objects.all()
+            serialize = LinesSerializer(lines, many=True)
+            return JsonResponse({"result": True, "items": serialize.data})
         else:
-            delete_props(line_item)
-            return JsonResponse({"result": True, "item": line_item})
+            line = save_line_item(line_item)
+            serialize = LinesSerializer(line)
+            return JsonResponse({"result": True, "item": serialize.data})
