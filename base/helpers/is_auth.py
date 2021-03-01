d7ca2ee315f1591f8fb72cd806df2a2b7e2b9a60
kot6egemot <Exos93009!!>
2021-03-01 15:56:23 +0300

    restrict user for roles

--- base/helpers/is_auth.py
+++ base/helpers/is_auth.py
@@ -7,15 +7,17 @@ class IsAuthenticated(permissions.IsAuthenticated):
         if request.method == 'OPTIONS':
             return True
         else:
-            entity = view._param_entity
-            if entity in ['bonus', 'line', 'constant', 'individual_change', 'cvs_upload']:
-                if not request.user.role.rule[view._param_entity]['view'] and request.method == 'GET':
-                   return False
-                if not request.user.role.rule[view._param_entity]['edit'] and request.method == 'POST':
-                   return False
-                if not request.user.role.rule[view._param_entity]['add'] and request.method == 'PUT':
-                   return False
-                if not request.user.role.rule[view._param_entity]['delete'] and request.method == 'DELETE':
-                   return False
+            if  hasattr (view, '_param_entity'):
+                if view._param_entity in ['bonus', 'line', 'constant', 'individual_change', 'cvs_upload']:
+                    print(request.user.role.rule[view._param_entity]['edit'])
+                    print(view._param_entity)
+                    if not request.user.role.rule[view._param_entity]['view'] and request.method == 'GET':
+                       return False
+                    if not request.user.role.rule[view._param_entity]['edit'] and request.method == 'POST':
+                       return False
+                    if not request.user.role.rule[view._param_entity]['add'] and request.method == 'PUT':
+                       return False
+                    if not request.user.role.rule[view._param_entity]['delete'] and request.method == 'DELETE':
+                       return False
         return super(IsAuthenticated, self).has_permission(request, view)
 
