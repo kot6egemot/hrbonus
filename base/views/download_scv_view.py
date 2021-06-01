24742b0eefe018876ce482889c0bc5fdab4194ae
kot6egemot <Exos93009!!>
2021-06-01 10:27:59 +0300

    fix response csv file

--- base/views/download_scv_view.py
+++ base/views/download_scv_view.py
@@ -1,12 +1,38 @@
 import csv
 import io
-from django.http import  FileResponse
+import os
+import zipfile
+
+import requests
+from django.http import FileResponse, HttpResponse, JsonResponse
+from idna import unicode
 from rest_framework.views import APIView
 from base.models import CSVExportView_Basic, Constant
 from base.serializers.bonus_serializer import ConstantsSerializer
 from base.serializers.csv_serializer import CSVExportView_BasicSerializer
 from base.views.utils import get_month_year
+import urllib.parse
+from django.http import StreamingHttpResponse
 
+
+# def getfiles(file):
+#     url = 'https://www.imgonline.com.ua/examples/bee-on-daisy.jpg'
+#     response = requests.get(url)
+#     # Get filename from url
+#     filename = os.path.split(url)[1]
+#     # Create zip
+#     buffer = io.BytesIO()
+#     zip_file = zipfile.ZipFile(buffer, 'w')
+#     print(filename)
+#     f = open('sample.csv', 'r')
+#     zip_file.write('sample.csv')
+#     f.close()
+#     zip_file.close()
+#     # Return zip
+#     response = HttpResponse(buffer.getvalue())
+#     response['Content-Type'] = 'application/x-zip-compressed'
+#     response['Content-Disposition'] = 'attachment; filename=album.zip'
+#     return response
 
 class DownloadCSVView(APIView):
     serializer_class = CSVExportView_BasicSerializer
@@ -25,11 +51,17 @@ class DownloadCSVView(APIView):
             many=True
         )
         headers = [field['name'] for field in CSVExportView_Basic.get_model_fields() if field['name'] not in ['ID', 'Year', 'Month']]
-        output = io.StringIO()
-        writer = csv.DictWriter(output, headers, delimiter=";")
-        writer.writeheader()
-        for stats in serializer.data:
-            writer.writerow(stats)
-        csv_output = output.getvalue()
-        response = FileResponse(csv_output, as_attachment=True)
+
+        with open("sample.csv", "w", encoding='utf-8-sig') as csv_file:
+            w = csv.DictWriter(csv_file, headers, delimiter=";")
+            w.writeheader()
+            for stats in serializer.data:
+                w.writerow(stats)
+
+        csv_file = open('sample.csv', 'rb')
+        response = FileResponse(csv_file)
         return response
+
+    def delete(self, request, *args, **kwargs):
+        os.remove('sample.csv')
+        return JsonResponse({'result': True})
